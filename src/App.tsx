91907e3b866198f2b10709ce963fb53e50141d79
NettoZk <adeuvaldon7@gmail.com>
2025-10-07 21:02:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import FormularioProjeto from './components/FormularioProjeto';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Formulário de Envio do Projeto</h1>
+      <FormularioProjeto />
+    </div>
+  );
+}
+
+export default App;
